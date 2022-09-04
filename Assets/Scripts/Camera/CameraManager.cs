6270bf7dbb539abf6e7115966689054b500112e6
OrangeP1anet <kass12092@outlook.jp>
2022-09-04 12:55:50 +0900

    Update CameraManager.cs

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public GameObject target;
+    public GameObject target; //追従対象
     Vector3 pos;
 
     void Start()
@@ -14,28 +14,13 @@ public class CameraManager : MonoBehaviour
 
     void Update()
     {
-        Vector3 cameraPos = target.transform.position; // cameraPosという変数を作り、追従する対象の位置を入れる
+        Vector3 cameraPos = target.transform.position;
 
-        // もし対象の横位置が0より小さい場合
-        if (target.transform.position.x < 0)
-        {
-            cameraPos.x = 0; // カメラの横位置に0を入れる
-        }
+        cameraPos.x = target.transform.position.x; // カメラの位置に対象の位置を代入
 
-        // もし対象の縦位置が0より小さい場合
-        if (target.transform.position.y < 0)
-        {
-            cameraPos.y = target.transform.position.y;   // カメラの縦位置に対象の位置を入れる
-        }
+        cameraPos.y = 0;  // 縦のカメラは固定
 
-        // もし対象の縦位置が0より大きい場合
-        if (target.transform.position.y > 0)
-        {
-            cameraPos.y = target.transform.position.y;   // カメラの縦位置に対象の位置を入れる
-        }
-
-        cameraPos.z = -10; // カメラの奥行きの位置に-10を入れる
-        Camera.main.gameObject.transform.position = cameraPos; //　カメラの位置に変数cameraPosの位置を入れる
-
+        cameraPos.z = -10;
+        Camera.main.gameObject.transform.position = cameraPos;
     }
 }
