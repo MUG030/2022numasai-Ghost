daa63573ed6da153011e587eb7836787ad622a38
OrangeP1anet <kass12092@outlook.jp>
2022-09-06 17:21:26 +0900

    仮のシーン遷移

--- Assets/Scripts/SceneChange/SceneChanger.cs
+++ Assets/Scripts/SceneChange/SceneChanger.cs
@@ -6,18 +6,33 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
 
-     /*void OnTriggerEnter2D(Collider2D col)
-     {
-         if (col.gameObject.tag == "Clear")//Clearのタグが付くオブジェクトに接触したらクリアシーンへの切り替え
-         {
-                SceneManager.LoadScene("ClearScene");
-                Debug.Log("Touch Goal");
-         }
+    public void SCTitle()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
 
-        if (col.gameObject.tag == "Enemy")//Clearのタグが付くオブジェクトに接触したらクリアシーンへの切り替え
+    public void SCMain()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void SCClear()
+    {
+        SceneManager.LoadScene("ClearScene");
+    }
+
+    /*void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Clear")//Clearのタグが付くオブジェクトに接触したらクリアシーンへの切り替え
         {
-            Debug.Log("Hit Enemy");
+               SceneManager.LoadScene("ClearScene");
+               Debug.Log("Touch Goal");
         }
 
-    }*/
+       if (col.gameObject.tag == "Enemy")//Clearのタグが付くオブジェクトに接触したらクリアシーンへの切り替え
+       {
+           Debug.Log("Hit Enemy");
+       }
+
+   }*/
 }
