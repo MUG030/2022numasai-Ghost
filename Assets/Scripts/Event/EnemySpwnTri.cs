d3f46f1ea8d91310b6ee64b4a918a4baa0e6ce06
OrangeP1anet <kass12092@outlook.jp>
2022-10-21 19:44:20 +0900

    イベント追加

--- Assets/Scripts/Event/EnemySpwnTri.cs
+++ Assets/Scripts/Event/EnemySpwnTri.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class EnemySpwnTri : MonoBehaviour
 {
-    public GameObject ghost;
+    public GameObject ghost1;
+    public GameObject ghost2;
+    public GameObject ghost3;
+    public GameObject ghost4;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            ghost.SetActive(true);
+            ghost1.SetActive(true);
+            ghost2.SetActive(true);
+            ghost3.SetActive(true);
+            ghost4.SetActive(true);
         }
     }
 }
