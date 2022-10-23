8d3e797cde4f15bb7f578361e3c90fb10064bd72
OrangeP1anet <kass12092@outlook.jp>
2022-10-23 15:58:23 +0900

    Revert "Revert "クリボーのような敵""
    This reverts commit 3526cde156a56cb0b00c8625e0a9d9f1de173cff.

---
+++ Assets/InoueDaisuke/Scripts/move.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class move : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    public float speed = 0.01f;
+    public float houkou = 1.0f;
+    public EnemyCollisionCheck checkCollision;
+    //private float p1=0.0f;
+    //public trigger checkCollision;
+    //private string enemyTag = "Enemy";
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        /*Vector3 hitPos;
+        foreach (ContactPoint point in other.contacts)
+        {
+            hitPos = point.point;
+            if (hitPos.y <0.5)
+            {
+                houkou = -1.0f;
+            }
+        }*/
+        /*if (other.gameObject.name == "BlockTile")
+        {
+            houkou = -1.0f;
+        }
+        //transform.localScale = new Vector3(houkou, 1, 1);
+        Debug.Log("butukatta");
+        */
+
+    }
+
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        houkou = -1.0f;
+    }*/
+    // Update is called once per frame
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    ///if(collision.collider.tag ==enemyTag)
+    ///{
+    /// Debug.Log("てきとぶつかった。");
+    /// }
+    //}
+    void Update()
+        {
+        if (checkCollision.isOn)
+        {
+            houkou= houkou*-1.0f;
+           
+        }
+            transform.Translate(houkou * speed, 0, 0);
+            transform.localScale = new Vector3(houkou, 1, 1);
+
+
+        
+
+
+        }
+ }
