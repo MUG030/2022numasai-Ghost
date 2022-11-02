33c801d2611c0759f13a56309b27742f41e57a04
OrangeP1anet <kass12092@outlook.jp>
2022-11-02 22:12:36 +0900

    Fadein

---
+++ Assets/Scripts/SceneChange/Fadein.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Fadein : MonoBehaviour
+{
+    public GameObject img;
+
+    public Ease Ease_Type;
+    public float duration = 2.0f;
+    public float start_alpha = 1f;
+    public float end_alpha = 0f;
+
+    void Start()
+    {
+        var img = GetComponent<Image>();
+        img.DOFade(end_alpha, duration)
+            .SetEase(Ease_Type);
+    }
+}
